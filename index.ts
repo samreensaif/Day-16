c1ddda2f5959b1440655e7136f9136741f0f5588
samreensaif <160466969+samreensaif@users.noreply.github.com>
2024-03-17 11:54:55 +0500

    Add files via upload
    Assignment # 46

---
+++ index.ts
@@ -0,0 +1,19 @@
+// Question 46: Enhanced Laptop Object: Construct an object for a laptop including properties make, model, year, and a method describe() that logs a sentence about the laptop.
+
+let laptop=
+   {
+    make : " HP",
+    model: "eliteBook",
+    year: 2024,
+    describe : function(){
+
+        
+        console.log(`This Laptop is ${laptop.make} model ${laptop.model} makes in the year ${laptop.year}` )
+       
+        console.log(`this laptop is ${this.make} ${this.model} in year ${this.year}` )
+
+        console.log(this.make, this.model,this.year )
+    }
+}
+
+laptop.describe()
